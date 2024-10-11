54e1d6fe4675b48f9a423d6922893cb879eb1f49
Luxurywebwide <lelite603@gmail.com>
2024-10-11 12:43:26 -0300

    Create script.js
    creando la carpeta de js

---
+++ js/script.js
@@ -0,0 +1,11 @@
+function entrar() {
+    document.getElementById("titulo").style="color: white"
+}
+
+function salir() {
+    document.getElementById("titulo").style="color: black"
+}
+
+function press(x) {
+    alert(x);
+}
